4e6ae1872d97ec291cd6e4e14d8ea127c2575e74
ryoga.isota <ryoga.8284@icloud.com>
2019-02-24 19:46:50 +0000

    CRUD tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # また、トップページはタスクリスト一覧と同じページにルーティングしてください。
+  root to: 'tasks#index'
+
+  # タスクの Router には resources を利用してください。
+  # resources で生成される全てのアクションを実装してください。
+  # →index, show, new, create, edit, update, destroyを実装するという意味
+  resources :tasks
 end
